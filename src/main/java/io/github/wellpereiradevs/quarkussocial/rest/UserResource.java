73e2c54f48c3e15409b111f41173b65d4bbb2178
wellpereiradevs <wellpereiradevs@gmail.com>
2023-12-14 14:45:02 -0300

    Changing Status Codes to More Specific Ones

--- src/main/java/io/github/wellpereiradevs/quarkussocial/rest/UserResource.java
+++ src/main/java/io/github/wellpereiradevs/quarkussocial/rest/UserResource.java
@@ -37,8 +37,8 @@ public class UserResource {
         Set<ConstraintViolation<CreateUserRequest>> violations = validator.validate(userRequest);
 
         if (!violations.isEmpty()) {
-            ResponseError responseError = ResponseError.createFromValidation(violations);
-            return Response.status(400).entity(responseError).build();
+            return ResponseError.createFromValidation(violations)
+                    .withStatusCode(ResponseError.UNPROCESSABLE_ENTITY_STATUS);
         }
 
         User user = new User();
@@ -47,7 +47,10 @@ public class UserResource {
 
         repository.persist(user);
 
-        return Response.ok(user).build();
+        return Response
+                .status(Response.Status.CREATED.getStatusCode())
+                .entity(user)
+                .build();
     }
 
     @GET
@@ -65,7 +68,7 @@ public class UserResource {
 
         if (user != null) {
             repository.delete(user);
-            return Response.ok().build();
+            return Response.noContent().build();
         }
         return Response.status(Response.Status.NOT_FOUND).build();
     }
@@ -80,7 +83,7 @@ public class UserResource {
         if (user != null) {
             user.setName(userData.getName());
             user.setAge(userData.getAge());
-            return Response.ok().build();
+            return Response.noContent().build();
         }
         return Response.status(Response.Status.NOT_FOUND).build();
     }
--- src/main/java/io/github/wellpereiradevs/quarkussocial/rest/dto/ResponseError.java
+++ src/main/java/io/github/wellpereiradevs/quarkussocial/rest/dto/ResponseError.java
@@ -1,6 +1,7 @@
 package io.github.wellpereiradevs.quarkussocial.rest.dto;
 
 import javax.validation.ConstraintViolation;
+import javax.ws.rs.core.Response;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
@@ -8,6 +9,7 @@ import java.util.stream.Collectors;
 
 public class ResponseError {
 
+    public static final int UNPROCESSABLE_ENTITY_STATUS = 422;
     private String message;
     private Collection<FieldError> errors;
 
@@ -40,5 +42,9 @@ public class ResponseError {
 
     public void setErrors(Collection<FieldError> errors) {
         this.errors = errors;
+    }
+
+    public Response withStatusCode(int code) {
+        return Response.status(code).entity(this).build();
     }
 }
