7975fadcbc48271400e7a111a5466a46dbc0ad71
wellpereiradevs <wellpereiradevs@gmail.com>
2023-12-14 10:08:18 -0300

    Receiving and Returning JSON Objects

--- src/main/java/io/github/wellpereiradevs/quarkussocial/rest/UserResource.java
+++ src/main/java/io/github/wellpereiradevs/quarkussocial/rest/UserResource.java
@@ -2,16 +2,23 @@ package io.github.wellpereiradevs.quarkussocial.rest;
 
 import io.github.wellpereiradevs.quarkussocial.rest.dto.CreateUserRequest;
 
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 
 @Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
 
     @POST
     public Response createUser(CreateUserRequest userRequest) {
+        return Response.ok(userRequest).build();
+    }
+
+    @GET
+    public Response listAllUsers() {
         return Response.ok().build();
     }
 }
