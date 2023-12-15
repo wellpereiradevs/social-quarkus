eae3310d64daf902a9dc0841a4448fbd269f58fc
wellpereiradevs <wellpereiradevs@gmail.com>
2023-12-15 11:37:46 -0300

    Completing the followers PUT

--- src/main/java/io/github/wellpereiradevs/quarkussocial/domain/repository/FollowerRepository.java
+++ src/main/java/io/github/wellpereiradevs/quarkussocial/domain/repository/FollowerRepository.java
@@ -1,10 +1,24 @@
 package io.github.wellpereiradevs.quarkussocial.domain.repository;
 
 import io.github.wellpereiradevs.quarkussocial.domain.model.Follower;
+import io.github.wellpereiradevs.quarkussocial.domain.model.User;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import io.quarkus.panache.common.Parameters;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.util.Optional;
 
 @ApplicationScoped
 public class FollowerRepository implements PanacheRepository<Follower> {
+
+    public boolean follows(User follower, User user) {
+
+        var params = Parameters.with("follower", follower).and("user", user).map();
+
+        PanacheQuery<Follower> query = find("follower =:follower and user =:user", params);
+        Optional<Follower> result = query.firstResultOptional();
+
+        return result.isPresent();
+    }
 }
--- src/main/java/io/github/wellpereiradevs/quarkussocial/rest/FollowerResource.java
+++ src/main/java/io/github/wellpereiradevs/quarkussocial/rest/FollowerResource.java
@@ -6,6 +6,7 @@ import io.github.wellpereiradevs.quarkussocial.domain.repository.UserRepository;
 import io.github.wellpereiradevs.quarkussocial.rest.dto.CreateFollowerRequest;
 
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -25,6 +26,7 @@ public class FollowerResource {
     }
 
     @PUT
+    @Transactional
     public Response followUser(@PathParam("userId") Long userId, CreateFollowerRequest followerRequest) {
         var user = userRepository.findById(userId);
         if (user == null) {
@@ -32,11 +34,15 @@ public class FollowerResource {
         }
         var follower = userRepository.findById(followerRequest.getFollowerId());
 
-        var entity = new Follower();
-        entity.setUser(user);
-        entity.setFollower(follower);
+        boolean follows = followerRepository.follows(follower, user);
 
-        followerRepository.persist(entity);
+        if (!follows) {
+            var entity = new Follower();
+            entity.setUser(user);
+            entity.setFollower(follower);
+
+            followerRepository.persist(entity);
+        }
 
         return Response.status(Response.Status.NO_CONTENT).build();
     }
