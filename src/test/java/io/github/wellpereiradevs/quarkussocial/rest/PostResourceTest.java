ec69f4e88877816cbf5939a5393efc23de34c42c
wellpereiradevs <wellpereiradevs@gmail.com>
2023-12-18 16:01:16 -0300

    Creating Scenario with BeforeEach

---
+++ src/test/java/io/github/wellpereiradevs/quarkussocial/rest/PostResourceTest.java
@@ -0,0 +1,54 @@
+package io.github.wellpereiradevs.quarkussocial.rest;
+
+import io.github.wellpereiradevs.quarkussocial.domain.model.User;
+import io.github.wellpereiradevs.quarkussocial.domain.repository.UserRepository;
+import io.github.wellpereiradevs.quarkussocial.rest.dto.CreatePostRequest;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.*;
+
+@QuarkusTest
+@TestHTTPEndpoint(PostResource.class)
+@Transactional
+class PostResourceTest {
+
+    @Inject
+    UserRepository userRepository;
+
+    Long userId;
+
+    @BeforeEach
+    public void setUP() {
+        var user = new User();
+        user.setAge(30);
+        user.setName("Fulano");
+        userRepository.persist(user);
+        userId = user.getId();
+    }
+
+    @Test
+    @DisplayName("should create a post for a user")
+    public void createPostTest() {
+        var postRequest = new CreatePostRequest();
+        postRequest.setText("Some text");
+        var userID = 1;
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(postRequest)
+                .pathParams("userId", userID)
+                .when()
+                .post()
+                .then()
+                .statusCode(201);
+    }
+}
