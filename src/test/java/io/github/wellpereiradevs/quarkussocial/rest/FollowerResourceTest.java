ac93d01e08f5236712ba39ad79d2103528bfe177
wellpereiradevs <132404088+wellpereiradevs@users.noreply.github.com>
2023-12-26 18:48:56 -0300

    Tests on the Followers API

---
+++ src/test/java/io/github/wellpereiradevs/quarkussocial/rest/FollowerResourceTest.java
@@ -0,0 +1,77 @@
+package io.github.wellpereiradevs.quarkussocial.rest;
+
+import io.github.wellpereiradevs.quarkussocial.domain.model.User;
+import io.github.wellpereiradevs.quarkussocial.domain.repository.UserRepository;
+import io.github.wellpereiradevs.quarkussocial.rest.dto.CreateFollowerRequest;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.ws.rs.core.Response;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.*;
+
+@QuarkusTest
+@TestHTTPEndpoint(FollowerResource.class)
+class FollowerResourceTest {
+
+    @Inject
+    UserRepository userRepository;
+
+    Long userId;
+
+    @BeforeEach
+    @Transactional
+    void setUp() {
+        //Standard test user
+        var user = new User();
+        user.setAge(30);
+        user.setName("Fulano");
+        userRepository.persist(user);
+        userId = user.getId();
+    }
+
+    @Test
+    @DisplayName("should return 409 when followerId is equal to  userId")
+    public void sameUserAsFollowerTest(){
+
+        var body = new CreateFollowerRequest();
+        body.setFollowerId(userId);
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(body)
+                .pathParam("userId", userId)
+                .when()
+                .put()
+                .then()
+                .statusCode(Response.Status.CONFLICT.getStatusCode())
+                .body(Matchers.is("You can't follow yourself"));
+    }
+
+    @Test
+    @DisplayName("should return 404 when userId doesn't exist")
+    public void userNotFoundTest(){
+
+        var body = new CreateFollowerRequest();
+        body.setFollowerId(userId);
+
+        var nonExistentUserId = 999;
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(body)
+                .pathParam("userId", nonExistentUserId)
+                .when()
+                .put()
+                .then()
+                .statusCode(Response.Status.NOT_FOUND.getStatusCode());
+    }
+}
